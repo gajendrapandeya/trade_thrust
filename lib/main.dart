95b30a16b732a4e4aa66c5cdd9f6cae9cce30bb2
gajendrapandeya <developerpandeya@gmail.com>
2021-12-06 14:07:16 +0545

    Added Logo of the app

--- lib/main.dart
+++ lib/main.dart
@@ -98,6 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
+            Image.asset('assets/icon/img.png'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
